fbad11db24dcdb0381beff395cad7c2876222e4b
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-05-14 19:05:43 -0500

    refactored code to reduce component files

--- react-developer-webpage/src/App.jsx
+++ react-developer-webpage/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header'
 
@@ -7,7 +6,6 @@ function App() {
   return (
     <>
       <Header />
-      <Outlet />
     </>
   )
 }
--- react-developer-webpage/src/components/Header.jsx
+++ react-developer-webpage/src/components/Header.jsx
@@ -1,27 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Outlet, Link } from 'react-router-dom'
 
-const Header = () => {
+function Navigation() {
+  return (
+    <ul className="nav nav-pills">
+        <li className='nav-item'>
+            <Link className='nav-link text-light' to="/">About Me</Link>
+        </li>
+        <li>
+            <Link className='nav-link text-light' to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+            <Link className='nav-link text-light' to="/contact">Contact</Link>
+        </li>
+        <li>
+            <Link className='nav-link text-light' to="/resume">Resumé</Link>
+        </li>
+    </ul>
+  )
+}
+
+function Header() {
   return (
     <div id='header' className='container'>
       <header className="d-flex flex-wrap just-content-center py-3 mb-4 border-bottom">
         <a href='/' className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
           <span className='fs-4 text-light fw-bold'>Alex Gael - Developer Portfolio</span>
         </a>
-        <ul className="nav nav-pills">
-          <li className='nav-item text'>
-            <Link className='nav-link text-light' to="/">About Me</Link>
-          </li>
-          <li>
-            <Link className='nav-link text-light' to="/portfolio">Portfolio</Link>
-          </li>
-          <li>
-            <Link className='nav-link text-light' to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link className='nav-link text-light' to="/resume">Resumé</Link>
-          </li>
-        </ul>
+        <Navigation />
+        <Outlet />
       </header>
     </div>
   )
