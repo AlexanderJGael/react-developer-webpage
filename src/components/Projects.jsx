3dd21ea96db87317c07d95da40159be4d2ee319d
Alex Gael <alexanderjgael@gmail.com>
2024-05-15 20:48:26 -0500

    added some projects to portfolio

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Project = ({ image, title, description }) => {
+const Project = ({ image, title, description, link }) => {
     return (
         <div className="col-md-6 pb-3">
-            <div className="h-100 p-4 text-bg-dark rounded-3">
-                <img className="img-fluid rounded mb-3" src={image} alt={title}/>
-                <h2 className="mx-3 text-start">{title}</h2>
+            <div id="projectCard" className="shadow-lg h-100 p-4 rounded-3">
+                <a href={link}><img className="img-fluid rounded mb-3" src={image} alt={title}/></a>
+                <h2 id="projectTitle" className="fw-bold mx-3 text-start">{title}</h2>
                 <p className="mx-3 text-start">{description}</p>
             </div>
         </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -19,12 +19,12 @@ const Portfolio = () => {
                     </div>
                 </div>
                 <div className="row align-items-md-stretch">
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
-                    <Project title="Project 1" image="https://via.placeholder.com/500x300" description="Description of project 1" />                
+                    <Project title="React Developer Website" link="https://github.com/AlexanderJGael/react-developer-webpage" image="https://via.placeholder.com/500x300" description="Developer portfolio website build using Vite, React and other technologies" />                
+                    <Project title="JATE: PWA Text Editor" link="https://github.com/AlexanderJGael/PWA-Text-Editor" image="https://via.placeholder.com/500x300" description="This is a progressive web application (PWA) that serves as a text editor, allowing users to create and edit notes or code snippets with or without an internet connection." />                
+                    <Project title="HarmonyHub" link="https://github.com/AlexanderJGael/harmony_hub/tree/bugfixes" image="https://via.placeholder.com/500x300" description='"For musicians, by musicians," this website is all about connecting with people who share an interest in creating and enjoying music.' />                
+                    <Project title="Weather Dashboard" link="https://github.com/AlexanderJGael/weather-app" image="https://github.com/AlexanderJGael/weather-app/blob/main/Assets/Screenshot%202024-01-31%20at%2017.15.07.png?raw=true" description="This project entails the creation of a weather dashboard application. The application runs in the browser and features dynamic updates to HTML and CSS." />                
+                    <Project title="Random Password Generator" link="https://github.com/AlexanderJGael/js-password-generator" image="https://via.placeholder.com/500x300" description="Description of project 4" />                
+                    <Project title="Project 6" image="https://via.placeholder.com/500x300" description="Description of project 6" />                
                 </div>
             </div>
         </section>
