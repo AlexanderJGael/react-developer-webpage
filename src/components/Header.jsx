a9b8f1fa0fb77ce92677ee74846ee2822fe37ffc
Alex Gael <alexanderjgael@gmail.com>
2024-05-15 20:24:13 -0500

    uniform design philosophy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -21,8 +21,8 @@ function Navigation() {
 function Header() {
   return (
       <header className="d-flex flex-wrap just-content-center py-3 mb-4">
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-          <span><img href='/' id='logo' className='mx-2 bi rounded-circle' height='32' src={logo}></img></span>
+        <a href="/" id='headerBrand' className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <span><img id='logo' className='mx-2 bi rounded-circle' height='32' src={logo}></img></span>
           <span className="fs-4 fw-bold text-light">Ally G. Designs</span>
         </a>
         <Navigation />
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,17 +4,17 @@ import Project from "../components/Projects"
 const Portfolio = () => {
     return (
         <section name="portfolio">
-            <div className="container py-4">
-                <div id="portfolioJumbo" className="p-5 mb-4 body-teritary rounded-3">
+            <div className="container my-5">
+                <div id="portfolioJumbo" className="shadow p-5 mb-4 rounded">
                     <div className="container-fluid py-5">
                         <h1 className="text-start display-5 fw-bold">portfolio</h1>
                         <p className="text-start col-md-8 fs-4">Check out some of my projects and follow the link below to see my GitHub repos or get in contact with me through my contact page!
                         <br />
                         Enjoy!
                         </p>
-                        <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button id="portfolioButton" className="btn btn-lg border rounded-pill text-light" type="button">GitHub</button>
-                            <button id="portfolioButton" className="btn btn-lg border rounded-pill text-light" type="button">Contact Me</button>
+                        <div id="buttonsDiv" className="d-grid gap-2 d-md-flex justify-content-md-start">
+                            <button id="portfolioButton" href="https://github.com/AlexanderJGael?tab=repositories" className="text-emphasis btn btn-lg border rounded-pill" type="button">GitHub</button>
+                            <button id="portfolioButton" href='/contact' className="btn btn-lg border rounded-pill" type="button">Contact Me</button>
                         </div>
                     </div>
                 </div>
