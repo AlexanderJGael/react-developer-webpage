665c09911fc1195ecfb89763646e65d4dddd2af4
Alex Gael <alexanderjgael@gmail.com>
2024-05-16 22:23:59 -0500

    finished portfolio page styling and animations

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,12 +1,20 @@
 import React from "react";
+import githubSVG from "../assets/github-mark-white.svg"
+import externalSVG from "../assets/externalLink.svg"
 
-const Project = ({ image, title, description, link }) => {
+const Project = ({ image, title, description, deployedLink, repoLink }) => {
     return (
         <div className="col-md-6 pb-3">
             <div id="projectCard" className="shadow-lg h-100 p-4 rounded-3">
-                <a href={link}><img className="img-fluid rounded mb-3" src={image} alt={title}/></a>
-                <h2 id="projectTitle" className="fw-bold mx-3 text-start">{title}</h2>
-                <p className="mx-3 text-start">{description}</p>
+                <div id="imageContainer" className="flex justify-content-center align-items-center">
+                    <img id="projectImage" className="img-fluid rounded mb-3" src={image} alt={title}/>
+                    <div id="linksDiv" className="overlay flex gap-4 align-items-center justify-content-center container-fluid">
+                        <a href={repoLink}><img className="bi svg-image" height="64px" src={githubSVG} alt='GitHub Logo'/></a>
+                        <a href={deployedLink}><img id="externalLink" height="80px" className="bi svg-image" src={externalSVG} alt='External Link'/></a>
+                    </div>
+                </div>
+                <h2 id="projectTitle" className="fw-bold text-start">{title}</h2>
+                <p className="text-start">{description}</p>
             </div>
         </div>
     )
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Project from "../components/Projects"
+import githubLogoWhite from "../assets/github-mark-white.svg"
+import externalLink from "../assets/externalLink.svg"
 
 const Portfolio = () => {
     return (
@@ -8,23 +10,20 @@ const Portfolio = () => {
                 <div id="portfolioJumbo" className="shadow p-5 mb-4 rounded">
                     <div className="container-fluid py-5">
                         <h1 className="text-start display-5 fw-bold">portfolio</h1>
-                        <p className="text-start col-md-8 fs-4">Check out some of my projects and follow the link below to see my GitHub repos or get in contact with me through my contact page!
-                        <br />
-                        Enjoy!
+                        <p className="text-start col-md-8 fs-4">Check out some of my projects and follow the link below to see my GitHub repositories!
                         </p>
-                        <div id="buttonsDiv" className="d-grid gap-2 d-md-flex justify-content-md-start">
-                            <button id="portfolioButton" href="https://github.com/AlexanderJGael?tab=repositories" className="text-emphasis btn btn-lg border rounded-pill" type="button">GitHub</button>
-                            <button id="portfolioButton" href='/contact' className="btn btn-lg border rounded-pill" type="button">Contact Me</button>
+                        <div id="buttonsDiv" className="d-grid pt-1 gap-2 d-md-flex justify-content-md-start">
+                            <a href="https://github.com/AlexanderJGael?tab=repositories" role="button" aria-pressed="true">
+                                <img id="portfolioButton"  src={githubLogoWhite} className="bi image-fluid" height="56" alt="Github" />
+                            </a>
                         </div>
                     </div>
                 </div>
-                <div className="row align-items-md-stretch">
-                    <Project title="react developer website" link="https://github.com/AlexanderJGael/react-developer-webpage" image="https://via.placeholder.com/500x300" description="Developer portfolio website build using Vite, React and other technologies" />                
-                    <Project title="jate: pwa text editor" deployed="https://jate-i2ev.onrender.com/" link="https://github.com/AlexanderJGael/PWA-Text-Editor" image="https://via.placeholder.com/500x300" description="This is a progressive web application (PWA) that serves as a text editor, allowing users to create and edit notes or code snippets with or without an internet connection." />                
-                    <Project title="harmonyhub" link="https://github.com/AlexanderJGael/harmony_hub/tree/bugfixes" image="https://via.placeholder.com/500x300" description='"For musicians, by musicians," this website is all about connecting with people who share an interest in creating and enjoying music.' />                
-                    <Project title="weather dashboard" link="https://github.com/AlexanderJGael/weather-app" image="https://github.com/AlexanderJGael/weather-app/blob/main/Assets/Screenshot%202024-01-31%20at%2017.15.07.png?raw=true" description="This project entails the creation of a weather dashboard application. The application runs in the browser and features dynamic updates to HTML and CSS." />                
-                    <Project title="random password generator" link="https://github.com/AlexanderJGael/js-password-generator" image="https://via.placeholder.com/500x300" description="Description of project 4" />                
-                    <Project title="project 6" image="https://via.placeholder.com/500x300" description="Description of project 6" />                
+                <div id="projectsDiv" className="row align-items-md-stretch">
+                    <Project title="jate: pwa text editor" deployed="https://jate-i2ev.onrender.com/" repoLink="https://github.com/AlexanderJGael/PWA-Text-Editor" deployedLink='#' image="https://via.placeholder.com/500x300" description="This is a progressive web application (PWA) that serves as a text editor, allowing users to create and edit notes or code snippets with or without an internet connection." />                
+                    <Project title="harmonyhub" repoLink="https://github.com/AlexanderJGael/harmony_hub/tree/bugfixes" deployedLink="#" image="https://via.placeholder.com/500x300" description='"For musicians, by musicians," this website is all about connecting with people who share an interest in creating and enjoying music.' />                
+                    <Project title="weather dashboard" repoLink="https://github.com/AlexanderJGael/weather-app" deployedLink="https://alexanderjgael.github.io/weather-app/" image="https://github.com/AlexanderJGael/weather-app/blob/main/Assets/Screenshot%202024-01-31%20at%2017.15.07.png?raw=true" description="This project entails the creation of a weather dashboard application. The application runs in the browser and features dynamic updates to HTML and CSS." />                
+                    <Project title="random password generator" repoLink="https://github.com/AlexanderJGael/js-password-generator" deployedLink="#" image="https://via.placeholder.com/500x300" description="Description of project 4" />                
                 </div>
             </div>
         </section>
