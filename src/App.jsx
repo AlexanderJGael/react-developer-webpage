92e4d1afdac1105ef4cb2c90823878586c19b54e
AlexanderJGael <130517352+AlexanderJGael@users.noreply.github.com>
2024-05-17 22:02:51 -0500

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,2 +1,25 @@
 import React from "react";
+import './css/footer.css'
+import githubLogoWhite from '../assets/github-mark-white.svg'
+import allyLogo from '../assets/logo.svg'
 
+export default function Footer() {
+    return (
+        <section className="footer">
+            <div className="container">
+            <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+                <div className="col-md-4 d-flex align-items-center">
+                <a href="/" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <img src={allyLogo} className="bi img-fluid  rounded-circle" width="30" height="24" />
+                </a>
+                <span className="mb-3 mb-md-0 text-body-light">© Alex Gael</span>
+                </div>
+
+                <ul className="nav col-md-4 align-items-center justify-content-end list-unstyled d-flex">
+                <li className="ms-3"><a className="text-body-secondary" href="http://github.com/AlexanderJGael"><img className="bi img-fluid" width="33" height="33" src={githubLogoWhite} alt='GitHub Logo'/></a></li>
+                </ul>
+            </footer>
+            </div>
+        </section>
+    )
+}
